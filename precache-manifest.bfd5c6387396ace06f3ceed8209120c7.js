a213757296f78c7a8f9f9f4280510e0acaf8f8c8
unknown <fitriandriyani94>
2019-08-15 12:03:11 +0700

    Updates

---
+++ precache-manifest.bfd5c6387396ace06f3ceed8209120c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4e9099de3e30d08592cc63d5a62021",
+    "url": "/workforcenew/index.html"
+  },
+  {
+    "revision": "ddd1047bd88742118094",
+    "url": "/workforcenew/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0bf23d8b83924ffbb33d",
+    "url": "/workforcenew/static/js/2.b6e086f5.chunk.js"
+  },
+  {
+    "revision": "ddd1047bd88742118094",
+    "url": "/workforcenew/static/js/main.655d3acd.chunk.js"
+  },
+  {
+    "revision": "bcd4a84eb1ecf1df85d6",
+    "url": "/workforcenew/static/js/runtime~main.a33dccef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workforcenew/static/media/logo.5d5d9eef.svg"
+  }
+]);
